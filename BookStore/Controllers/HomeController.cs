934255f5ff5ee1d2e1f94a74ae26ee4c51ba908b
Muhammad Muso <musogulomov123@gmail.com>
2023-02-16 21:46:05 +0500

    CODE RUB: Delete ErrorViewModel

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-using BookStore.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
@@ -26,7 +24,7 @@ namespace BookStore.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
