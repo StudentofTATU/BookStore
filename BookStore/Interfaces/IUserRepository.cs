7b6e6f88b83317b1b8f9578a9dfcedbe570d1260
Muhammad Muso <musogulomov123@gmail.com>
2023-02-17 22:50:12 +0500

    Create User Repository

---
+++ BookStore/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookStore.Models;
+
+namespace BookStore.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<User> GetUserByIdAsync(int userId);
+
+        bool Add(User user);
+        bool Update(User user);
+        bool Delete(User user);
+        bool Save();
+    }
+}
